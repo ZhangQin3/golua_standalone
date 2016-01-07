8138dac76f6276f52a276e4b92dabec90c127259
Zhang Qin <zhang.ethan3@gmail.com>
2016-01-07 11:22:03 +0800

    And an example model

--- lua.go
+++ lua.go
@@ -1,9 +1,12 @@
 package main
 
-import "golua/lua"
 import "os"
+import "golua/lua"
+import "golua_standalone/model"
 
 func main() {
 	L := lua.NewState()
+	// Register go functions test and test2 in ./model/mod.go file to lua
+	mod.Register(L)
 	lua.MainGo(L, os.Args)
 }
---
+++ model/mod.go
@@ -0,0 +1,23 @@
+package mod
+
+import "fmt"
+import "golua/lua"
+
+func test(L *lua.State) int {
+	fmt.Println("hello world! from go!")
+	return 0
+}
+
+func test2(L *lua.State) int {
+	arg := L.CheckInteger(-1)
+	argfrombottom := L.CheckInteger(1)
+	fmt.Print("test2 arg: ")
+	fmt.Println(arg)
+	fmt.Print("from bottom: ")
+	fmt.Println(argfrombottom)
+	return 0
+}
+
+func Register(L *lua.State) {
+	L.RegisterFuncs("mod", lua.GoFuncs{"test2": test2, "test": test})
+}
