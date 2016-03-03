fc1884f368dfe3d62521b32b048e729c3ccd69ae
Zhang Qin <zhang.ethan3@gmail.com>
2016-03-03 19:03:28 +0800

    archive

--- ct.lua
+++ ct.lua
@@ -1,33 +1,288 @@
 
-function foo()
-	print("-----------in2")
-	coroutine.yield()
-	print("-----------hh2")
+function sleep(t)
+	c = t/5
+	for i = 1, c, 1 do
+		coth.sleep(5)
+	end
 end
 
-
-
-print("------before")
-cot.start(function()
-		for i = 1, 20, 1 do
-			cot.sleep(10)
-			print("============= " .. i)
-		end
-	end)
-
-cot.start(function()
-		for i = 1, 20, 1 do
-			cot.sleep(15)
-			print("----------------- " .. i)
-			print("----------------- " .. i)
-		end
-	end)
-
-print("------after")
-
--- for i = 1, 30, 1 do
--- 	cot.sleep(30)
--- 	print("------------------------sleep ".. i)
+-- function foo()
+-- 	print("-----------in2")
+-- 	coroutine.yield()
+-- 	print("-----------hh2")
 -- end
 
-cot.sleep(5000)
+-- coth.start(function()
+-- 		for i = 1, 20, 1 do
+-- 			-- coth.sleep(10)
+-- 			sleep(10)
+-- 			print("================== " .. i)
+
+-- 		end
+-- 	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("0000000000000000000 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("1111111111111111111111 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("222222222222222222222222 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("333333333333333333333333 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("0000000000000000000 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("1111111111111111111111 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("222222222222222222222222 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("333333333333333333333333 " .. i)
+		end
+	end)
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("0000000000000000000 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("1111111111111111111111 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("222222222222222222222222 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("333333333333333333333333 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("0000000000000000000 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("1111111111111111111111 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("222222222222222222222222 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("333333333333333333333333 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("0000000000000000000 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("1111111111111111111111 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("222222222222222222222222 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("333333333333333333333333 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("0000000000000000000 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("1111111111111111111111 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("222222222222222222222222 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("333333333333333333333333 " .. i)
+		end
+	end)
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("0000000000000000000 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("1111111111111111111111 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("222222222222222222222222 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("333333333333333333333333 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("0000000000000000000 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("1111111111111111111111 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("222222222222222222222222 " .. i)
+		end
+	end)
+
+coth.start(function()
+		for i = 1, 20, 1 do
+			-- coth.sleep(15)
+			sleep(15)
+			print("333333333333333333333333 " .. i)
+		end
+	end)
+-- for i = 1, 30, 1 do
+-- 	coth.sleep(30)
+-- 	print("------------------------coth.sleep ".. i)
+-- end
+
+s = os.time()
+sleep(2000)
+e = os.time()
+
+print(s, e)
+print(e-s)
+-- coth.coth.sleep(5000)
